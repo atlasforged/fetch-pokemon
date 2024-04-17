2b54652f7d149ac4ff2b0206f6322c12499dfdca
atlasforged <148302823+atlasforged@users.noreply.github.com>
2024-04-17 16:02:17 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+fetchData();
+
+async function fetchData() {
+	try {
+		const pokemonName = document
+			.getElementById('pokemonName')
+			.value.toLowerCase();
+
+		const response = await fetch(
+			`https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+		);
+
+		if (!response.ok) {
+			throw new Error('Could not fetch resource');
+		}
+
+		const data = await response.json();
+
+		const pokemonSprite = data.sprites.front_default;
+		const imgElement = document.getElementById('pokemonSprite');
+
+		imgElement.src = pokemonSprite;
+		imgElement.style.display = 'block';
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// Adding ability to press enter to search in addition to mouse click
+document.onkeydown = (event) => {
+	if (event.key === 'Enter') {
+		fetchData();
+	}
+};
